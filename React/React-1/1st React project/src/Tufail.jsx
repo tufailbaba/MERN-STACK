45f1afc4f9272696cd77ca38c4c7d370d8e78fd5
Tufail <tufailbabacoc@gmail.com>
2026-03-26 14:21:23 +0530

    Added first tag

---
+++ React/React-1/1st React project/src/Tufail.jsx
@@ -0,0 +1,12 @@
+
+function Tufail() { 
+    return(
+        <div>
+            <h1>My First React Tag-Tufail</h1>
+            <p>This is a simple React application.</p>
+         
+        </div>
+    );
+}
+
+export default Tufail;
