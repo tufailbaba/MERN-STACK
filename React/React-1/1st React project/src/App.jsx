16ee5a2c6b38617370c6448c61c2ba9872e5c831
Tufail <tufailbabacoc@gmail.com>
2026-03-26 14:27:57 +0530

    Added 2nd  tag

--- React/React-1/1st React project/src/App.jsx
+++ React/React-1/1st React project/src/App.jsx
@@ -1,9 +1,11 @@
 import Tufail  from "./Tufail"; 
+import Baba from "./Baba"
 function App(){
   return(
     <div>
       <h1>My First React App</h1>
       <Tufail></Tufail> 
+      <Baba></Baba>
     </div>
   );
 }
---
+++ React/React-1/1st React project/src/Baba.jsx
@@ -0,0 +1,8 @@
+function Baba (){
+    return (
+        <div>
+            <h1>Hi,this is my 2nd tag</h1>
+        </div>
+    )
+}
+export default Baba
