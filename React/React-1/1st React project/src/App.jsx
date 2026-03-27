d5ac48676d22086c107d5b124166ee41b7c58229
Tufail <tufailbabacoc@gmail.com>
2026-03-27 11:06:26 +0530

    Functions

--- React/React-1/1st React project/src/App.jsx
+++ React/React-1/1st React project/src/App.jsx
@@ -1,18 +1,46 @@
-import Tufail  from "./Tufail"; 
+import { useState } from "react";
+import Tufail from "./Tufail"; 
 import Table from "./table";
-import Baba from "./Baba"
-import New from "./new"
-function App(){
-  return(
+import Baba from "./Baba";
+import New from "./new";
+
+function App() {
+  const [b, setB] = useState(0);
+  const [a, setA] = useState(0);
+
+  const inc = () => {
+    setA(prev => prev + 1);
+    setB(prev => prev + 1);
+  };
+
+  const dec = () => {
+    setA(prev => prev - 1);
+  };
+
+  return (
     <div>
       <h1>My First React App</h1>
-      <Tufail>This is my 1st tag</Tufail> 
-      <Tufail>This is my 2nd tag</Tufail> 
-      <Baba></Baba>
+
+      <h1>B: {b}</h1>
+
+      <button onClick={inc}>+</button>
+      <br /><br />
+
+      <h1>A: {a}</h1>
+      <button onClick={dec}>-</button>
+
+      <hr />
+
+      <Tufail>This is my 1st tag</Tufail>
+
       <Table first={1} second={2} />
-      <New></New>
+
+      {/* Optional components */}
+      {/* <Baba /> */}
+      {/* <New /> */}
+
     </div>
   );
 }
-export default App;
 
+export default App;
